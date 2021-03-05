7bc70b8b27892562675c4dc8ca3da64f62e1cf9c
333Kenji <333kenji@gmail.com>
2021-03-05 14:57:00 -0500

    Added src dir and abstraction for creating df_main

---
+++ src/data_merger.py
@@ -0,0 +1,128 @@
+
+
+
+# amfAR
+
+#load Amfar opioid and HIV data, add county code
+opiod_df = pd.read_table('data/amfAR/countydata.tsv',header=0)
+opiod_df['county_code'] = opiod_df.STATEFP*1000 + opiod_df.COUNTYFP # build a county code column
+opiod_df['county_code'] = opiod_df.county_code.astype(int)
+
+
+#make changes to the amfar dataframe
+#convert from long to wide format
+index_lst = ['county_code', 'COUNTY', 'STATEABBREVIATION', 'YEAR']
+col_lst = ['INDICATOR']
+opiod_df_wide = opiod_df.pivot_table(values='VALUE', index=index_lst, columns=col_lst).reset_index()
+
+
+# Focus on just the HIV related data, from 2008 onward
+opiod_df_wide = opiod_df_wide[opiod_df_wide['YEAR'] >= 2008] # subset for years that have hiv data
+cols_to_drop = ['CDC_consult', 'vulnerable_rank']
+#, 'num_SSPs', 'bup_phys', 'drugdep', 'pctunmetneed', 'nonmedpain']
+opiod_df_wide.drop(cols_to_drop, axis=1, inplace=True) # drop unnecessary columns
+
+
+# By default pandas doesn't show all the columns (shows ... instead). Set max columns so that all can be inspected.
+pd.get_option("display.max_columns")
+pd.set_option("display.max_columns", 100)
+
+
+# Subset data to 2015
+opiod_df_15 = opiod_df_wide[opiod_df_wide['YEAR'] == 2015]
+
+pd.options.mode.chained_assignment = None  # default='warn', get rid of annoying warning associated with dropping below
+# drop columns having no 2015 data, will be extraploated from following years later
+opiod_df_15.drop(['num_SSPs', 'bup_phys', 'drugdep', 'pctunmetneed', 'nonmedpain'], axis=1, inplace=True)
+
+
+# get esimates for num_SSPs, bug_phys, drug_dep, pctunmetneed, and nonmedpain from following years
+
+#subset opioid related data from one year only
+#number of needle exchange programs (num_SSPs)
+opiod_df_wide_17 = opiod_df_wide[opiod_df_wide['YEAR'] == 2017]
+df_num_SSP = opiod_df_wide_17[['num_SSPs', 'county_code']]
+
+#number of doctors licensed to rx Buprenorphine (bup_phys)
+df_bup_phys = opiod_df_wide_17[['bup_phys', 'county_code']]
+
+#percent with drug dependency (drug_dep)
+opiod_df_wide_16 = opiod_df_wide[opiod_df_wide['YEAR'] == 2016]
+df_drugdep = opiod_df_wide_16[['drugdep', 'county_code']]
+
+#percent unmet drug treatment need (pctunmetneed)
+df_pctunmetneed = opiod_df_wide_16[['pctunmetneed', 'county_code']]
+
+#percent taken pain meds for nonmedical use (nonmedpain)
+df_nonmedpain = opiod_df_wide_16[['nonmedpain', 'county_code']]
+
+
+# merge these values back into 2015 dataframe
+#merge opioid related data back to the 2015 dataframe
+opiod_df_15 = opiod_df_15.merge(df_num_SSP, on='county_code')
+opiod_df_15 = opiod_df_15.merge(df_bup_phys, on='county_code')
+opiod_df_15 = opiod_df_15.merge(df_drugdep, on='county_code')
+opiod_df_15 = opiod_df_15.merge(df_pctunmetneed, on='county_code')
+opiod_df_15 = opiod_df_15.merge(df_nonmedpain, on='county_code')
+
+
+
+# CAMP Data
+#load Men who have sex with men (MSM) estimate data
+msm_df = pd.read_csv("data/CAMP/US_MSM_Estimates_Data_2013.csv")    #load the data
+msm_df['county_code'] = msm_df.STATEFP*1000 + msm_df.COUNTYFP  # build a county code column
+msm_df['county_code'] = msm_df.county_code.astype(int)
+msm_df['%msm12month'] = 100 * (msm_df.MSM12MTH / msm_df.ADULTMEN) # build a %MSM within last 12 months column
+msm_df['%msm5yr'] = 100 * (msm_df.MSM5YEAR / msm_df.ADULTMEN)     # build a %MSM within last 5years column
+
+
+cols_to_drop = ['REGCODE', 'DIVCODE', 'STATEFP', 'COUNTYFP', 'CSACODE', 
+                'CBSACODE','METDCODE', 'METMICSA', 'CENTOUTL']
+msm_df.drop(cols_to_drop, axis=1, inplace=True) #drop all unneeded columns
+
+
+
+
+#### ACS Data
+
+#unemplyment data
+df_employment = pd.read_csv("data/ACS_14_5YR_employment/ACS_14_5YR_S2301_with_ann.csv", 
+                            encoding = "ISO-8859-1", skiprows=1)
+df_employment = df_employment[['Id2', 'Unemployment rate; Estimate; Population 16 years and over']]
+df_employment.columns = ['county_code', 'unemployment_rate']
+
+
+#poverty data
+df_poverty = pd.read_csv("data/ACS_14_5YR_poverty/ACS_14_5YR_S1701_with_ann.csv", 
+                         encoding = "ISO-8859-1", skiprows=1)
+df_poverty = df_poverty[['Id2', 'Percent below poverty level; Estimate; Population for whom poverty status is determined']]
+df_poverty.columns = ['county_code', 'poverty_rate']
+
+
+
+#income data
+df_income = pd.read_csv("data/ACS_14_5YR_income/ACS_14_5YR_S1901_with_ann.csv", 
+                        encoding = "ISO-8859-1", skiprows=1)
+df_income = df_income[['Id2', 'Households; Estimate; Total']]
+df_income.columns = ['county_code', 'household_income']
+
+
+
+#Insurance data
+df_insurance = pd.read_csv("data/ACS_14_5YR_health_insur_coverage/ACS_14_5YR_S2701_with_ann.csv", 
+                        encoding = "ISO-8859-1", skiprows=1)
+df_insurance = df_insurance[['Id2', 'Total; Estimate; SEX - Male', 'Number Uninsured; Estimate; SEX - Male']]
+df_insurance.columns = ['county_code', 'Insured Males', 'Uninsured Males']
+
+
+
+
+### MERGING ALL OF THE THINGS
+#merge asfAR hiv/opioid data with CAMP MSM data
+df_main = opiod_df_15.merge(msm_df, on='county_code')
+
+#merge in ACS data
+df_main = df_main.merge(df_employment, on='county_code')
+df_main = df_main.merge(df_poverty, on='county_code')
+df_main = df_main.merge(df_income, on='county_code')
+df_main = df_main.merge(df_insurance, on='county_code')
